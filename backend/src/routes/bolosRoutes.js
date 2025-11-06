6ec4696793ea9cb308f220a7b471924ca1c47bf0
Alan Ortiz <aortizcabrero@student.cvtc.edu>
2025-11-06 10:39:41 -0600

    Implement bolo api routes

--- backend/src/routes/bolosRoutes.js
+++ backend/src/routes/bolosRoutes.js
@@ -1,20 +1,22 @@
 import express from "express";
-import {
-  getBoloList,
-  getAllBolos,
-  getBolo,
-  createBolo,
-  updateBolo,
-  deleteBolo,
-} from "../controllers/bolosController.js";
+import * as bolosController from "../controllers/bolosController.js";
 
 const router = express.Router();
 
-router.get("/", getBoloList);
-router.get("/all/", getAllBolos);
-router.get("/:id", getBolo);
-router.post("/", createBolo);
-router.put("/:id", updateBolo);
-router.delete("/:id", deleteBolo);
+// api/bolos/
+router.get("/", bolosController.getBoloSummary);
+router.get("/:id", bolosController.getBolo);
+router.post("/", bolosController.createBolo);
+router.put("/:id", bolosController.updateBolo);
+router.delete("/:id", bolosController.deleteBolo);
+
+// api/bolos/vehicles/
+router.post("/:bolo_id/vehicles/", bolosController.createBoloVehicle);
+router.delete("/:bolo_id/vehicles/:id", bolosController.deleteBoloVehicle);
+
+// api/bolos/persons/
+router.post("/:bolo_id/persons/", bolosController.createBoloPerson);
+router.put("/:bolo_id/persons/:id", bolosController.updateBoloPerson);
+router.delete("/:bolo_id/persons/:id", bolosController.deleteBoloPerson);
 
 export default router;
